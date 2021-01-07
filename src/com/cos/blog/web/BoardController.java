cb6fc4ca81c397b2bcbfae30a8b2d3df5abaed39
unknown <qwq140@gmail.com>
2021-01-07 11:42:40 +0900

    10 JSP블로그 글쓰기 구현 완

--- src/com/cos/blog/web/BoardController.java
+++ src/com/cos/blog/web/BoardController.java
@@ -6,6 +6,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.cos.blog.domain.board.dto.SaveReqDto;
+import com.cos.blog.domain.user.User;
+import com.cos.blog.service.BoardService;
+import com.cos.blog.util.Script;
+
+
 
 
 @WebServlet("/board")
@@ -26,7 +34,33 @@ public class BoardController extends HttpServlet {
 	}
 
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		String cmd = request.getParameter("cmd");
+		BoardService boardService = new BoardService();
+		HttpSession session = request.getSession();
+		if(cmd.equals("saveForm")) {
+			User principal = (User)session.getAttribute("principal");
+			if(principal != null) {
+				response.sendRedirect("board/saveForm.jsp");
+			} else {
+				response.sendRedirect("user/loginFrom.jsp");
+			}
+		} else if (cmd.equals("save")) {
+			int userId = Integer.parseInt(request.getParameter("userId"));
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			
+			SaveReqDto dto = new SaveReqDto();
+			dto.setUserId(userId);
+			dto.setTitle(title);
+			dto.setContent(content);
+			int result = boardService.글쓰기(dto);
+			
+			if (result == 1 ) { // 정상
+				response.sendRedirect("index.jsp");
+			} else {
+				Script.back(response, "글쓰기실패");
+			}
+		}
 	}
 
 }
